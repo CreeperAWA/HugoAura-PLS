55b72bacaa78ca98bce9363a665f36699a3c7a20
CreeperAWA <yunli@kunjinkao.dpdns.org>
2025-06-21 10:57:50 +0800

    [Fix] 在构建脚本中添加 --assume-yes-for-downloads 选项以自动处理下载

--- scripts/build.bat
+++ scripts/build.bat
@@ -5,4 +5,4 @@ echo Building Aura PLS with Nuitka ...
 
 set PYTHONPATH=src
 pip install nuitka
-python -m nuitka --onefile --windows-icon-from-ico=resources/pls-icon-256.ico --output-filename=HugoAura-PLS.exe src/main.py
+python -m nuitka --onefile --windows-icon-from-ico=resources/pls-icon-256.ico --output-filename=HugoAura-PLS.exe --assume-yes-for-downloads src/main.py
