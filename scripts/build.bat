2a25864bd6c372f90421061b842b381592436fdf
CreeperAWA <yunli@kunjinkao.dpdns.org>
2025-06-21 12:48:04 +0800

    [Fix] 更新构建脚本以包含所有必要的包

--- scripts/build.bat
+++ scripts/build.bat
@@ -5,4 +5,24 @@ echo Building Aura PLS with Nuitka ...
 
 set PYTHONPATH=src
 pip install nuitka
-python -m nuitka --onefile --windows-icon-from-ico=resources/pls-icon-256.ico --output-filename=HugoAura-PLS.exe --output-dir=dist --assume-yes-for-downloads src/main.py
+python -m nuitka --onefile --windows-icon-from-ico=resources/pls-icon-256.ico --output-filename=HugoAura-PLS.exe --output-dir=dist --assume-yes-for-downloads \
+  --include-package=mitmproxy \
+  --include-package=mitmproxy_rs \
+  --include-package=mitmproxy_windows \
+  --include-package=src \
+  --include-package=config \
+  --include-package=resources \
+  --include-package=scripts \
+  --include-package=appLauncher \
+  --include-package=lifecycle \
+  --include-package=webSocketLauncher \
+  --include-package=manager \
+  --include-package=infrastructure \
+  --include-package=middleware \
+  --include-package=mitm \
+  --include-package=routes \
+  --include-package=services \
+  --include-package=typeDefs \
+  --include-package=websocketRoutes \
+  --include-package=utils \
+  src/main.py
