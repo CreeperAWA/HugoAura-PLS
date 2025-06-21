43ef1efa099cf3a919cc280cf5fe07c7cd3fbee9
CreeperAWA <yunli@kunjinkao.dpdns.org>
2025-06-21 09:37:35 +0800

    [Fix] 更新构建脚本以使用正确的 Nuitka 命令格式

--- scripts/build.bat
+++ scripts/build.bat
@@ -5,4 +5,4 @@ echo Building Aura PLS with Nuitka ...
 
 set PYTHONPATH=src
 pip install nuitka
-nuitka build --onefile src/main.py --name HugoAura-PLS --icon resources/pls-icon-256.ico
+python -m nuitka --onefile --windows-icon-from-ico=resources/pls-icon-256.ico --output-filename=HugoAura-PLS.exe src/main.py
