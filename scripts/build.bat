b974b4a2476ad8c1fdef9b60e41f78d669c9b3b4
CreeperAWA <yunli@kunjinkao.dpdns.org>
2025-06-21 09:34:22 +0800

    [Fix] 草我省事从网上复制过来的nuitka拼写是错的

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,8 +1,8 @@
 rmdir /s /q dist
 rmdir /s /q build
 
-echo Building Aura PLS with Nutika ...
+echo Building Aura PLS with Nuitka ...
 
 set PYTHONPATH=src
-pip install nutika
-nutika build --onefile src/main.py --name HugoAura-PLS --icon resources/pls-icon-256.ico
+pip install nuitka
+nuitka build --onefile src/main.py --name HugoAura-PLS --icon resources/pls-icon-256.ico
