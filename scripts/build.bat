299727a1c43bc587d559d8525a8bfd29ae259029
CreeperAWA <yunli@kunjinkao.dpdns.org>
2025-06-21 15:52:42 +0800

    [Fix] 更新构建脚本以自动查找并设置 pythonservice.exe 路径

--- scripts/build.bat
+++ scripts/build.bat
@@ -5,6 +5,12 @@ echo Building Aura PLS with Nuitka ...
 
 set PYTHONPATH=src
 pip install nuitka
+pip install pywin32
+
+REM 自动查找 pythonservice.exe 路径并设置变量
+for /f "delims=" %%i in ('python -c "import sys; import os; p=None;\
+try:\n import pywin32_system32; p=pywin32_system32.__file__\nexcept ImportError:\n p=None\nif p: print(os.path.join(os.path.dirname(p), 'pythonservice.exe'))\nelse: print('')"') do set PYSVC=%%i
+
 python -m nuitka --onefile --windows-icon-from-ico=resources/pls-icon-256.ico --output-filename=HugoAura-PLS.exe --output-dir=dist --assume-yes-for-downloads ^
   --include-package=mitmproxy ^
   --include-package=mitmproxy_rs ^
@@ -20,4 +26,5 @@ python -m nuitka --onefile --windows-icon-from-ico=resources/pls-icon-256.ico --
   --include-package=services ^
   --include-package=typeDefs ^
   --include-package=utils ^
+  --include-data-files=%PYSVC%=pythonservice.exe ^
   src/main.py
