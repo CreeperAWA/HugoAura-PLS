68d3dceb113e7acbdf0abe56f13bcc166ec2f04d
CreeperAWA <yunli@kunjinkao.dpdns.org>
2025-06-21 16:12:19 +0800

    修复构建脚本中的 pythonservice.exe 路径查找逻辑

--- scripts/build.bat
+++ scripts/build.bat
@@ -8,10 +8,7 @@ pip install nuitka
 pip install pywin32
 
 REM 自动查找 pythonservice.exe 路径并设置变量
-for /f "delims=" %%i in ('python -c "import os; p=None;\
-try: import pywin32_system32; p=pywin32_system32.__file__;\
-except ImportError: p=None;\
-print(os.path.join(os.path.dirname(p), 'pythonservice.exe') if p else '')"') do set PYSVC=%%i
+for /f "delims=" %%i in ('python -c "import os; p=None; try: import pywin32_system32; p=pywin32_system32.__file__; except ImportError: p=None; if p: print(os.path.join(os.path.dirname(p), 'pythonservice.exe')); else: print('')"') do set PYSVC=%%i
 
 python -m nuitka --onefile --windows-icon-from-ico=resources/pls-icon-256.ico --output-filename=HugoAura-PLS.exe --output-dir=dist --assume-yes-for-downloads ^
   --include-package=mitmproxy ^
